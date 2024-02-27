a63ee48c61bcf27dcc85d7b07b8db1a3a4717730
amalsayedm <amal.sayedm2@gmail.com>
2024-02-28 01:46:51 +0200

    task0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author amal sayed
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
